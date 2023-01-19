0109f0175efa465b7631d1cf382d9324d4faaea8
Gabriel Klein <gabriel.klein@philips.com>
2023-01-19 05:15:14 -0300

    Created the component PageHeader

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,9 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
-  return 'New Contact page';
+  return (
+    <PageHeader
+      title="Novo contato"
+    />
+  );
 }
