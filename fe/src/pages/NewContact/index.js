65cdfc26dad40ace7fc5fd9ed6799a1f10596069
Gabriel Klein <gabriel.klein@philips.com>
2023-01-20 05:32:19 -0300

    Created the Input, select and button components

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,9 +1,26 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">insta</option>
+        <option value="123">insta</option>
+        <option value="123">insta</option>
+        <option value="123">insta</option>
+        <option value="123">insta</option>
+      </Select>
+      <Button type="button">Salvar alterações</Button>
+      <Button type="button" disabled>Salvar alterações</Button>
+    </>
   );
 }
