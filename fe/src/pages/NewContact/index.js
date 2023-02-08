4ae10a3db9daef401693a89023f4a6f58b6235f5
Gabriel Klein <gabriel.klein@philips.com>
2023-02-08 07:18:17 -0300

    Implemented the contact creation

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,8 +1,28 @@
+/* eslint-disable react/jsx-no-bind */
 import PageHeader from '../../components/PageHeader';
 
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+
+      };
+
+      const response = await ContactsService.createContact(contact);
+
+      console.log(response);
+    } catch (error) {
+      alert('ocorreu um erro ao cadastrar o contato!');
+    }
+  }
+
   return (
     <>
       <PageHeader
@@ -11,6 +31,7 @@ export default function NewContact() {
 
       <ContactForm
         buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
       />
     </>
   );
